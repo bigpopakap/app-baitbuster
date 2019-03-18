3f60b1d340dc12fd53476e39902aa6b2d60de99d
Kapil Easwar <kapil@squareup.com>
2019-03-18 11:04:05 -0700

    Add TODO

--- chrome-ext/background.js
+++ chrome-ext/background.js
@@ -4,6 +4,8 @@ chrome.webRequest.onBeforeRequest.addListener(async (details) => {
   // TODO figure out how to synchronously redirect so that we don't start loading the site
   //      ex. return { redirectUrl: '...' };
 
+  // TODO don't do any redirects if we're already on or are going to the baitbuster subreddit
+
   // Asynchronously redirect. This means we'll have already made the request to load the site,
   // but hopefully this will be quick enough that it won't be too jarring to the user.
   const redirectUrl = await getBuster(details.url);
